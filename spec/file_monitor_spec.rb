8c87f1221e6f8372af2b658c178f22989815e01d
Mark Billie <mbillie1@gmail.com>
2014-08-03 13:31:18 -0600

    updating tests for ruby 2.1.1 / rspec

--- spec/file_monitor_spec.rb
+++ spec/file_monitor_spec.rb
@@ -6,15 +6,15 @@ describe FileMonitor do
 
   it 'monitors a directory in order to observe new files' do
     system('touch ~/forker/monitor/sample_file')
-    @fm.changes?.should be_true
+    expect(@fm.changes?).to eq(true)
   end
 
   it 'can accept a new baseline' do
     system('touch ~/forker/monitor/sample_file')
     @fm.baseline!
-    @fm.changes?.should be_false
+    expect(@fm.changes?).to eq(false)
     system('touch ~/forker/monitor/sample_file_two')
-    @fm.changes?.should be_true
+    expect(@fm.changes?).to eq(true)
   end
 
   it 'is not thrown off by repeated editing of the same file' do
@@ -24,13 +24,13 @@ describe FileMonitor do
     @fm.baseline!
 
     system('touch ~/forker/monitor/sample_file')
-    @fm.changes?.should be_false
+    expect(@fm.changes?).to eq(false)
   end
 
   it 'knows which files are new' do
     system('touch ~/forker/monitor/new_file')
-    @fm.changes?.should be_true
-    @fm.changes.should eq ['new_file']
+    expect(@fm.changes?).to eq(true)
+    expect(@fm.changes).to eq ['new_file']
   end
 end
 
--- spec/fork_foreman_spec.rb
+++ spec/fork_foreman_spec.rb
@@ -8,7 +8,7 @@ describe ForkForeman do
       name, cmd = 'mark', ''
       expect(@ff).to receive(:fork).and_return(123)
       expect(@ff).to receive(:kill_if_exists).with(name)
-      Process.stub(:detach)
+      allow(Process).to receive(:detach)
       @ff.spawn_process(name, cmd)
     end
   end
@@ -19,7 +19,7 @@ describe ForkForeman do
       @ff.instance_variable_set(:@pids, { name => pid })
       expect(Process).to receive(:kill).with('SIGTERM', pid)
       @ff.kill_if_exists(name)
-      @ff.pids[name].should be_nil
+      expect(@ff.pids[name]).to eq(nil)
     end
   end
 end
